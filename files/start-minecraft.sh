d8e0384e3c2d27355d1332c8a7b3a5791bf03148
Joey Triska <jdtriska@gmail.com>
2020-04-15 22:11:39 -0400

    Enable attaching to MC server

--- files/start-minecraft.sh
+++ files/start-minecraft.sh
@@ -2,6 +2,7 @@
 sudo service docker restart
 docker ps -aq --filter "name=minecraft" | grep -q . && docker stop minecraft && docker rm -fv minecraft
 docker run -d \
+ -it \
  -e EULA=TRUE \
  -e TYPE=FORGE \
  -e TYPE=PAPER -e VERSION=1.15.2 \
