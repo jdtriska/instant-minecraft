6f5461095cf4b2ef9e7f46a534f348dbf20f1c7c
Joseph Triska <jdtriska@gmail.com>
2022-03-21 00:06:55 -0400

    updated for compatibility with latest mc version and latest paper mc version

--- files/start-minecraft.sh
+++ files/start-minecraft.sh
@@ -5,9 +5,9 @@ docker run -d \
  -it \
  -e EULA=TRUE \
  -e FORCE_REDOWNLOAD=true \
- -e TYPE=PAPER -e VERSION=1.15.2 \
+ -e TYPE=PAPER -e VERSION=1.15.2 -e PAPER_DOWNLOAD_URL=https://papermc.io/api/v2/projects/paper/versions/1.18.2/builds/393/downloads/paper-1.18.2-265.jar \
  -p 25565:25565 \
  --volume ~/minecraft/data:/data \
  --restart=unless-stopped \
  --name minecraft \
- itzg/minecraft-server
+ itzg/minecraft-server:latest
